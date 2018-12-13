123b382d8d17ea0dd5a153f4e3b7cc2ca881e63f
Blackstone Engineering <austin.blackstone@arm.com>
2018-12-13 16:31:49 -0600

    changed default repo

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-# import os
-# import tdclient
+ import os
+ import tdclient
+ import logging
+
+# configure logging level. 
+logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.INFO)
 
 # # Treasure Data Information
 # apikey = "10389/599f0416d91cfba4fe1d950e3497e01be258efec" 
 # database = "device_health"
 # query = "select AVG( cast(total_size as double)) FROM heap_info"
 
+################################
 # # Step 2 - Get info from Treasure Data 
+################################
 # with tdclient.Client(apikey) as client:
 #     job = client.query(database, query)
 #     print "Runnig TD query... Please wait for it to finish (<30sec)..."
@@ -16,27 +23,76 @@
 #     for row in job.result():
 #         print(row)
 
+################################
 # # Step 3 - Custom Algorithm
+################################
 
 # 	# get the avg value
 # x = 0
 # for thing in job.result():
 # 	x = thing
-# value = x[0] 
+# algo_value = x[0] 
 
+################################
 # Step 4 - Compile new binary with custom #define's
-from online_compiler_script import *
-mbedUser = "mbed_demo"
-mbedPassword = "password2"
-# repo = "https://os.mbed.com/teams/mbed_example/code/Compile_API";
-repo="https://os.mbed.com/users/coisme/code/Pelion-DM-Workshop-Project"
-# target = "ST-Discovery-L475E-IOT01A";
-target = "ST-Discovery-L475E-IOT01A"
-symbols = "MESSAGE=\"Hello World\",LED=LED2"
+################################
+# from online_compiler_script import *
+# from connect_dev_credentials import *
+# mbedUser = "mbed_demo"
+# mbedPassword = "password2"
+# # repo = "https://os.mbed.com/teams/mbed_example/code/Compile_API";
+# # repo = "https://os.mbed.com/teams/mbed-os-examples/code/mbed-os-example-blinky"
+# repo="https://os.mbed.com/users/mbed_demo/code/e2edemo-base"
+# # target = "ST-Discovery-L475E-IOT01A";
+# target = "ST-Discovery-L475E-IOT01A"
+# # symbols = "MESSAGE=\"Hello World\",LED=LED2"+","+connect_macros.replace('\n','')
+# symbols = ""
+# build_repo(mbedUser,mbedPassword,repo,target,symbols)
 
-build_repo(mbedUser,mbedPassword,repo,target,symbols)
+import subprocess
+
+mbedos_repo="https://www.github.com/BlackstoneEngineering/mbed-os-example-e2e-demo"
+mbedos_repo_projectname = mbedos_repo.split("/")[-1]
+logging.debug("project name detected as "+mbedos_repo_projectname)
+target = "DISCO_L475VG_IOT01A"
+algo_name = "ALGO" # this corresponds to the #define in the main.cpp file
+mbed_cloud_api_key = "CHANGE THIS"
+mbed_cloud_device_id = "CHANGE THIS"
+
+#Check that repo is downloaded
+if not os.isdir(mbedos_repo_projectname):
+	logging.info("Repo not found, importing it now")
+	check = subprocess.check_output("mbed","import",mbedos_repo)
+	logging.debug("[MBED IMPORT]"+check)
+
+os.chdir(mbedos_repo_projectname) # change working directory to be inside mbedos repo
+
+# check if update certs are in the project.
+if not os.isdir(".update-certificates"):
+	# If no update certs, add them (only first time)
+	check = subprocess.check_output("mbed", "dm", "init", "-a", mbed_cloud_api_key, "-d", "\"http://os.mbed.com\"", "--model-name", "\"modelname\"", "-q", "--force")
+	logging.debug("[Mbed Device Management]"+check)
+
+# Compile the program with algorightm added
+logging.info("Adding #define "+algo_name+"="+algo_value+"to the binary")
+check=subprocess.check_output("mbed", "compile", "--target", target,"--toolchain", "GCC_ARM","-D"+algoname+"="+algo_value)
+logging.debug("[Mbed Compile]"+check)
+
+
+################################
+# Step 5 - Send new binary to device via Pelion
+################################
+
+# set Mbed CLoud API key into the environment
+logging.info("setting mbed cloud api key into the environment variables")
+os.environ["CLOUD_SDK_API_KEY"]=mbed_cloud_api_key
+check = subprocess.check_output("mbed", "config", "CLOUD_SDK_API_KEY", mbed_cloud_api_key)
+
+# issue update
+# logging.info("running update on device "+mbed_cloud_device_id)
+# check = subprocess.check_output("mbed", "dm" ,"update", "device", "-D", mbed_cloud_device_id, "-m", target , "-vv")
+# logging.debug("[mbed dm update]"+check)
+
 
 
 print("finished")
-
-# Step 5 - Send new binary to device via Pelion
